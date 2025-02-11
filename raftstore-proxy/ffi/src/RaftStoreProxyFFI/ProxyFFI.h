b955b23956270a0db29135ea72db35822084ba0b
Calvin Neo <calvinneo1995@gmail.com>
2025-02-11 19:35:43 +0800

    to revert
    Signed-off-by: Calvin Neo <calvinneo1995@gmail.com>

--- raftstore-proxy/ffi/src/RaftStoreProxyFFI/ProxyFFI.h
+++ raftstore-proxy/ffi/src/RaftStoreProxyFFI/ProxyFFI.h
@@ -394,10 +394,14 @@ struct EngineStoreServerHelper {
                                           ReportThreadAllocateInfoBatch data);
 };
 
+#ifdef __cplusplus
 extern "C" {
+#endif
 // Basically same as ffi_server_info, but no need to setup ProxyHelper, only
 // need to setup ServerHelper. Used when proxy not start.
 uint32_t ffi_get_server_info_from_proxy(intptr_t, BaseBuffView, RawVoidPtr);
+#ifdef __cplusplus
 }
+#endif
 
 }  // namespace DB

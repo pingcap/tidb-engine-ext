75ac9881048aa79f77770c8e58c79563916071ea
Calvin Neo <calvinneo1995@gmail.com>
2025-02-12 10:34:16 +0800

    to revert
    Signed-off-by: Calvin Neo <calvinneo1995@gmail.com>

--- gen-proxy-ffi/src/lib.rs
+++ gen-proxy-ffi/src/lib.rs
@@ -128,6 +128,7 @@ pub fn gen_ffi_code() {
         .derive_copy(false)
         .enable_cxx_namespaces()
         .disable_header_comment()
+        .emit_clang_ast()
         .default_enum_style(bindgen::EnumVariation::Rust {
             non_exhaustive: false,
         });
--- raftstore-proxy/ffi/src/RaftStoreProxyFFI/ProxyFFI.h
+++ raftstore-proxy/ffi/src/RaftStoreProxyFFI/ProxyFFI.h
@@ -405,3 +405,4 @@ uint32_t ffi_get_server_info_from_proxy(intptr_t, BaseBuffView, RawVoidPtr);
 #endif
 
 }  // namespace DB
+void testaaa();

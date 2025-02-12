e79a8ae4787e9ca4f42c7f9eabdc37fb745260e9
Calvin Neo <calvinneo1995@gmail.com>
2025-02-12 21:07:43 +0800

    use
    Signed-off-by: Calvin Neo <calvinneo1995@gmail.com>

--- proxy_scripts/ci_check.sh
+++ proxy_scripts/ci_check.sh
@@ -2,7 +2,7 @@ set -uxeo pipefail
 if [[ $M == "fmt" ]]; then
     pwd
     git rev-parse --show-toplevel
-    make gen_proxy_ffi_fast
+    make gen_proxy_ffi
     git status -s .
     GIT_STATUS=$(git status -s .) && if [[ ${GIT_STATUS} ]]; then echo "Error: found illegal git status"; echo ${GIT_STATUS}; [[ -z ${GIT_STATUS} ]]; fi
     cargo fmt -- --check

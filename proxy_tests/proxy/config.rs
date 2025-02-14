db1ab72ee1296fd8a3941470c6d33667819402df
Calvin Neo <calvinneo1995@gmail.com>
2025-02-14 13:54:27 +0800

    a
    Signed-off-by: Calvin Neo <calvinneo1995@gmail.com>

--- proxy_tests/proxy/config.rs
+++ proxy_tests/proxy/config.rs
@@ -127,8 +127,6 @@ fn test_config_proxy_default_no_config_item() {
     assert_eq!(config.raft_store.evict_cache_on_memory_ratio, 0.1);
     assert_eq!(config.memory_usage_high_water, 0.9);
     assert_eq!(config.server.reject_messages_on_memory_ratio, 0.05);
-
-    assert_eq!(config.raft_store.enable_v2_compatible_learner, true);
 }
 
 #[test]

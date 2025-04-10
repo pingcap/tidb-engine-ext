dc7c2bcee1712d582b6eb500d5675a734a52ef95
Calvin Neo <calvinneo1995@gmail.com>
2025-04-10 10:34:46 +0800

    remove some comments 2
    Signed-off-by: Calvin Neo <calvinneo1995@gmail.com>

--- components/raftstore/src/store/worker/region.rs
+++ components/raftstore/src/store/worker/region.rs
@@ -639,9 +639,7 @@ where
 
         let region_state = self.region_state(*region_id)?;
         let apply_state = self.apply_state(*region_id)?;
-
         check_abort(&abort)?;
-
         let term = apply_state.get_truncated_state().get_term();
         let idx = apply_state.get_truncated_state().get_index();
         let snap_key = SnapKey::new(*region_id, term, idx);

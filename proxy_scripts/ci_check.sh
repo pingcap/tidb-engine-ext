c4867af653fa0e9c9a65d3ca23df14a8127d80b7
Calvin Neo <calvinneo1995@gmail.com>
2025-05-23 16:50:34 +0800

    try fix
    Signed-off-by: Calvin Neo <calvinneo1995@gmail.com>

--- proxy_scripts/ci_check.sh
+++ proxy_scripts/ci_check.sh
@@ -2,7 +2,8 @@ set -uxeo pipefail
 if [[ $M == "fmt" ]]; then
     pwd
     git rev-parse --show-toplevel
-    make gen_proxy_ffi
+    # lock file version 4 was found ..., so can only use default version
+    make gen_proxy_ffi_fast
     git status -s .
     GIT_STATUS=$(git status -s .) && if [[ ${GIT_STATUS} ]]; then echo "Error: found illegal git status"; echo ${GIT_STATUS}; [[ -z ${GIT_STATUS} ]]; fi
     cargo fmt -- --check
